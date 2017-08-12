3c78dcb5f0eea91509f3fcd8018ea22cf12bf756
HIM <hv0905@outlook.com>
2017-08-12 22:59:38 +0800

    添加项目文件。

---
+++ BaidupanDelFree/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace BaidupanDelFree
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 8);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(475, 579);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "文件列表";
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(3, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(469, 554);
+            this.listBox1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.listBox1, "拖动到此处也可添加哦");
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(116, 593);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 33);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "添加文件夹";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(378, 593);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 33);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "清空";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 593);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "添加文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(270, 593);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 33);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "删除选定项";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(8, 632);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(972, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(489, 8);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(491, 245);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "设定";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(450, 107);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(34, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "···";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(172, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(312, 26);
+            this.textBox3.TabIndex = 9;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(82, 134);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 24);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.Text = "指定目录";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(82, 103);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(177, 24);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "源目录(不会覆盖源文件)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "保存路径";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 194);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(478, 37);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "开始";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 164);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(98, 24);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "完成后关机";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(252, 77);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(67, 26);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "混淆码之前的空字节数量（推荐默认）";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(7, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(478, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "自定义混淆码（空白为随机生成（推荐），编码：UTF-8）";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBox1);
+            this.groupBox3.Location = new System.Drawing.Point(487, 259);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(493, 367);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "日志姬";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 22);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(487, 342);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(225, 593);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(39, 33);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "i";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 668);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "百度云资源反和谐flag工具1.0 -- HV0905 Studio";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ BaidupanDelFree/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BaidupanDelFree
+{
+    public partial class Form1 : Form
+    {
+
+        public List<string> _files = new List<string>();
+        private byte[] _passcode;
+        private uint _nullBytCount;
+        private Thread _workThread;
+        private bool _running;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(MessageBox.Show("确定要清空所有项目吗？","Warning",MessageBoxButtons.YesNo,MessageBoxIcon.Warning,MessageBoxDefaultButton.Button2) == DialogResult.No) return;
+            _files.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            int i = listBox1.SelectedIndex;
+            if(i == -1) return;
+            _files.RemoveAt(listBox1.SelectedIndex);
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            listBox1.SelectedIndex = i-1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog
+            {
+                Description = "选择目录",
+                ShowNewFolderButton = true
+            };
+            if (fbd.ShowDialog() == DialogResult.OK) {
+                AddDir(fbd.SelectedPath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog
+            {
+                Filter = "所有文件|*.*",
+                AddExtension = false,
+                CheckFileExists = true,
+                CheckPathExists = true,
+                Multiselect = true,
+            };
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                AddFiles(ofd.FileNames);
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e) => MessageBox.Show(
+                "原理：\n本程序是通过在文件末端增加随机字节（或指定字节）来更改文件的sha-1值，从而使百度的文件检测机制无法奏效。这对大多数已知文件类型（包括所有类型的视频文件）不会产生影响，但是对于部分文件类型（例如txt文本文档）会导致末端出现奇怪字符。\n制作：HV0905 Studio\n官方网站：hv0905.github.io\n喜欢就请给我们一个like，或者捐助一下\n最后，祝使用愉快q(≧▽≦q)\n                                                    ---HIM 2017", "关于");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var fbd = new FolderBrowserDialog
+            {
+                Description = "选择保存位置",
+                ShowNewFolderButton = true
+            };
+            if (fbd.ShowDialog() == DialogResult.OK) {
+                textBox3.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            if(e.Data.GetDataPresent(DataFormats.FileDrop,true) || e.Data.GetDataPresent(DataFormats.Locale, true))
+            e.Effect = DragDropEffects.Copy;
+            else e.Effect = DragDropEffects.None;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            //foreach (var item in e.Data.GetFormats(true)) {
+            //    Debug.WriteLine(item);
+            //    if (item. DataFormats.FileDrop)
+            //    {
+            //        AddFiles(e.Data.GetData(DataFormats.FileDrop) as string);
+            //    }
+            //}
+            AddFiles(((string[])e.Data.GetData(DataFormats.FileDrop,true)));
+            AddDir(((string[])e.Data.GetData(DataFormats.FileDrop,true)));
+
+        }
+
+
+        private void AddDir(params string[] path)
+        {
+            foreach (var item in path) {
+                if(!Directory.Exists(item)) continue;
+                var childDir = new List<string> {item};
+                for (int i = 0; i < childDir.Count; i++)
+                {
+                    try
+                    {
+                        childDir.AddRange(Directory.GetDirectories(childDir[i]));
+                    } catch { }
+                }
+                var files = new List<string>();
+                foreach (var item_ in childDir)
+                {
+                    try
+                    {
+                        files.AddRange(Directory.GetFiles(item_));
+                    } catch { }
+                }
+                AddFiles(files.ToArray());
+            }
+        }
+
+        private void AddFiles(params string[] files)
+        {
+            foreach (var item in files) {
+                if(!File.Exists(item)) continue;
+                if(_files.Exists(a => a == item)) continue;
+                listBox1.Items.Add(item);
+                _files.Add(item);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (_running)
+            {
+                _workThread.Abort();
+                _running = false;
+                button1.Enabled = button2.Enabled = button3.Enabled = button4.Enabled = button7.Enabled
+                    = numericUpDown1.Enabled = radioButton1.Enabled = radioButton2.Enabled = true;
+                textBox2.ReadOnly = textBox3.ReadOnly = false;
+                button5.Text = "开始";
+                //remove items
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    if (((string) listBox1.Items[i]).StartsWith("[进行中]"))
+                    {
+                        listBox1.Items[i] = ((string)listBox1.Items[i]).Substring(5);
+                        continue;
+                    }
+                    if (!((string)listBox1.Items[i]).StartsWith("[已完成]")) continue;
+                    _files.RemoveAt(i);
+                    listBox1.Items.RemoveAt(i);
+                    i--;
+                }
+            }
+            else
+            {
+                if (listBox1.Items.Count == 0)
+                {
+                    MessageBox.Show("任务列表中无项目", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if ((!radioButton1.Checked) && (!Directory.Exists(textBox3.Text)))
+                {
+                    MessageBox.Show("存储路径不存在！", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                _passcode = string.IsNullOrEmpty(textBox2.Text)
+                    ?Guid.NewGuid().ToByteArray()
+                    : Encoding.UTF8.GetBytes(textBox2.Text);
+                _nullBytCount = (uint) numericUpDown1.Value;
+                button1.Enabled = button2.Enabled = button3.Enabled = button4.Enabled = button7.Enabled
+                    = numericUpDown1.Enabled = radioButton1.Enabled = radioButton2.Enabled = false;
+                textBox2.ReadOnly = textBox3.ReadOnly = true;
+                progressBar1.Value = 0;
+                progressBar1.Maximum = _files.Count;
+                button5.Text = "停止";
+                _workThread = new Thread(RunWork);
+                _workThread.Start();
+                _running = true;
+            }
+        }
+
+
+        private void RunWork()
+        {
+            textBox1.AppendText("开始sha-1混淆\r\n");
+            for (int i = 0; i < _files.Count; i++)
+            {
+                try
+                {
+                    textBox1.AppendText($"[{i}]{_files[i]}正在混淆.\r\n");
+                    listBox1.Items[i] = "[进行中]" + _files[i];
+                    var source = File.Open(_files[i], FileMode.Open);
+                    var target =
+                        File.Open(
+                            radioButton1.Checked
+                                ? Path.Combine(Path.GetDirectoryName(_files[i]),
+                                    Path.GetFileNameWithoutExtension(_files[i]) + " - 反和谐" +
+                                    Path.GetExtension(_files[i]))
+                                : Path.Combine(textBox3.Text, Path.GetFileName(_files[i])), FileMode.Create);
+                    source.CopyTo(target);
+                    source.Close();
+                    target.Write(new byte[_nullBytCount],0,(int)_nullBytCount );
+                    target.Write(_passcode,0,_passcode.Length);
+                    target.Flush();
+                    target.Close();
+                    textBox1.AppendText($"[{i}]{_files[i]}混淆成功.\r\n");
+                    listBox1.Items[i] = "[已完成]" + _files[i];
+                    listBox1.SelectedIndex = i;
+                }
+                catch
+                {
+                    textBox1.AppendText($"[{i}]{_files[i]}读取错误，跳过。\r\n");
+                }
+                progressBar1.Value++;
+            }
+            textBox1.AppendText("所有操作完成。\r\n");
+            button1.Enabled = button2.Enabled = button3.Enabled = button4.Enabled = button7.Enabled
+                = numericUpDown1.Enabled = radioButton1.Enabled = radioButton2.Enabled = true;
+            textBox2.ReadOnly = textBox3.ReadOnly = false;
+            button5.Text = "开始";
+            _files.Clear();
+            listBox1.Items.Clear();
+            _running = false;
+            if (checkBox1.Checked)
+                Process.Start("shutdown", "-s -t 30");
+        }
+    }
+}
